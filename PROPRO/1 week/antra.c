4b9afbbcb029c97c359f16f90104226ac9a90707
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-09-12 11:19:02 +0300

    Update 2uzd

--- PROPRO/1 week/antra.c
+++ PROPRO/1 week/antra.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(){
-    printf("Leonardas\nSinkevicius\n""1 kursas\n""1 kursas\n");
+    printf("Leonardas\nSinkevicius\n1 kursas\n1 grupe\n");
+    printf("         ______________\n");
+    printf("        /             /|\n");
+    printf("       /             / |\n");
+    printf("      /____________ /  |\n");
+    printf("     | ___________ |   |\n");
+    printf("     ||           ||   |\n");
+    printf("     ||           ||   |\n");
+    printf("     ||           ||   |\n");
+    printf("     ||___________||   |\n");
+    printf("     |   _______   |  /\n");
+    printf("    /|  (_______)  | /\n");
+    printf("   ( |_____________|/\n");
+    printf("    \\\n");
+    printf(".=======================.\n");
+    printf("| ::::::::::::::::  ::: |\n");
+    printf("| ::::::::::::::[]  ::: |\n");
+    printf("|   -----------     ::: |\n");
+    printf("`-----------------------'\n");
     return 0;
 }
