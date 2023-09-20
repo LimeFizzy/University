c183eb9770e2af9a78bfa36d48bc8c2d11f3d5b6
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-09-20 13:49:18 +0300

    Upd 2 week 1 uzd

--- PROPRO/2 week/1uzd.c
+++ PROPRO/2 week/1uzd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("%c%c%c%c%c%c%c%c%c\n", 76, 101, 111, 110, 97, 114, 100, 97, 115);
+    char vardas[9] ={76, 101, 111, 110, 97, 114, 100, 97, 115};
+    for(int i=0; i<9; i++) printf("%c", vardas[i]);
     return 0;
 }
