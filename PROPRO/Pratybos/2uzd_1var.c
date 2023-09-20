23471033cf162b13e8c727316c931730192b801a
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-09-20 13:34:21 +0300

    Add 2uzd

---
+++ PROPRO/Pratybos/2uzd_1var.c
@@ -0,0 +1,20 @@
+//LSP numeris - 2312091
+//el. pastas - leonardas.sinkevicius@mif.stud.vu.lt
+//Uzduoties numeris - 2
+//Uzduoties variantas - 1
+
+//Įvesti sveiką skaičių N. Užpildyti matricą N*N sveikais skaičiais 1, 2, 3, ..., surašytais spirale. 
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    int numbers[N*N];
+    for(int i=1; i<=pow(N, 2); i++) {
+        numbers[i-1]=i;
+        printf("%d ", numbers[i-1]);
+    }
+    return 0;
+}
