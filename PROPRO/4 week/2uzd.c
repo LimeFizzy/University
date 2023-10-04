a6722e7103e6ec25c1d8b9ee29eaabfd6a564966
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-10-04 22:59:24 +0300

    Add 4 week 2 task

---
+++ PROPRO/4 week/2uzd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    printf("Programa atspausdina visus teigiamus sveikus skaičius iš intervalo (a; b], kurie dalijasi iš skaičiaus c su liekana 1.\n");
+    printf("Iveskite sveikuosius skacius a, b ir c: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    for(a; a<=b; ++a){
+        if(a%c==1){
+            printf("%d ", a);
+        }
+    }
+    return 0;
+}
