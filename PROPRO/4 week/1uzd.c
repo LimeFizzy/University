8e8d4ba8285a4b3eabfcf905cf9442cb9d48d543
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-10-04 22:52:22 +0300

    Add 4 week tasks

---
+++ PROPRO/4 week/1uzd.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double a, b, c, D, x1, x2;
+    printf("Programa sprendzia kvadratines lygtis.\n");
+    printf("Iveskite lygties koeficientus a, b ir c:\n");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if(a!=0){
+        D = sqrt(pow(b,2) - 4*a*c);
+        if(D>0){
+            x1 = (-b+D) / (2.0*a);
+            x2 = (-b-D) / (2.0*a);
+            printf("Yra 2 sprendimai: %.2lf ir %.2lf\n", x1, x2);
+        }
+        else if(D==0){
+            x1 = -b / (2.0*a);
+            printf("Yra 1 sprendimas: %.2lf\n", x1);
+        }
+        else 
+            printf("Sprendiniu nera.\n");
+    }
+    else   
+        printf("Sprendiniu nera.\n");
+
+    return 0;
+}
