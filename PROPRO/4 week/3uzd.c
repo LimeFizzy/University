93df59ab426f5ef0a22def33a7490c767ba2e8cf
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-10-05 12:12:40 +0300

    Add 4 week 3 task

---
+++ PROPRO/4 week/3uzd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int c;
+    int FibonaciNum[100]={0};
+    printf("Programa isveda c-ati fibonaci sekos skaiciu.\n");
+    printf("Iveskite 3 neneigiamus sveikuosius skaicius a, b ir c.\n");
+    scanf("%d %d %d", &FibonaciNum[0], &FibonaciNum[1], &c);
+    for(int i=0; i+2<=c; ++i){
+        FibonaciNum[i+2]=FibonaciNum[i]+FibonaciNum[i+1];
+    }
+    printf("%d-asis fibonaci sekos skaicius yra %d.\n", c, FibonaciNum[c]);
+    return 0;
+}
