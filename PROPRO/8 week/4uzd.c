f8f9c0a794103cdee629d3ef5ff154b246a2aedb
Leonardas <141874292+LimeFizzy@users.noreply.github.com>
2023-10-31 12:32:01 +0200

    Update 4uzd.c
    Final 4 task 8 week

--- PROPRO/8 week/4uzd.c
+++ PROPRO/8 week/4uzd.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int splitData(int array[], int arraySize, int split, int *first, int *second);
+int splitData(int array[], int arraySize, int split, int **first, int **second);
 
 int main(){
     int *first = NULL, *second = NULL;
     int masyvas[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    int errorcode = splitData(masyvas, 10, 2, first, second);
+    int errorcode = splitData(masyvas, 10, 2, &first, &second);
     return 0;
 }
 
-int splitData(int array[], int arraySize, int split, int *first, int *second){
+int splitData(int array[], int arraySize, int split, int **first, int **second){
     if(first == NULL && second == NULL && arraySize >=0 && split <= arraySize && split >= 0){
-        first = (int *)malloc(split * sizeof(int));
-        second = (int *)malloc((arraySize - split) * sizeof(int));
+        *first = (int *)malloc(split * sizeof(int));
+        *second = (int *)malloc((arraySize - split) * sizeof(int));
         
         for(int i = 0; i < split; ++i){
-            first[i] = array[i];
+            (*first)[i] = array[i];
             printf("%d ", first[i]);
         }
 
         for(int k = 0; split < arraySize; ++split, ++k){
-            second[k] = array[split];
+            (*second)[k] = array[split];
         }
         return 0;
     }
