b440781c4bdaeceb34ebb1c49e73edebd0503da2
Leonardas SInkevicius <141874292+LimeFizzy@users.noreply.github.com>
2023-11-01 12:05:50 +0200

    Update 4uzd.c
    8 weeks tasks ready

--- PROPRO/8 week/4uzd.c
+++ PROPRO/8 week/4uzd.c
@@ -5,19 +5,21 @@ int splitData(int array[], int arraySize, int split, int **first, int **second);
 
 int main(){
     int *first = NULL, *second = NULL;
-    int masyvas[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    int errorcode = splitData(masyvas, 10, 2, &first, &second);
+    int masyvas[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, arrSize = 10, split = 2;
+    int errorcode = splitData(masyvas, arrSize, split, &first, &second);
+    
+    free(first);
+    free(second);
     return 0;
 }
 
 int splitData(int array[], int arraySize, int split, int **first, int **second){
-    if(first == NULL && second == NULL && arraySize >=0 && split <= arraySize && split >= 0){
+    if(*first == NULL && *second == NULL && arraySize >=0 && split <= arraySize && split >= 0){
         *first = (int *)malloc(split * sizeof(int));
         *second = (int *)malloc((arraySize - split) * sizeof(int));
         
         for(int i = 0; i < split; ++i){
             (*first)[i] = array[i];
-            printf("%d ", first[i]);
         }
 
         for(int k = 0; split < arraySize; ++split, ++k){
