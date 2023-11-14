cf7069660f7f63954cba7227c691f0db7daf6969
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-11-14 15:26:01 +0200

    changes

--- PRAKINFO/moores-law.py
+++ PRAKINFO/moores-law.py
@@ -1,10 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 14:20:14 2023
+
+@author: lesi9952
+"""
+import matplotlib.pyplot as plt
+
+
+class Processor:
+    def __init__(self, name, transistors, year):
+        self.name = name
+        self.transistors = transistors
+        self.year = year
+    def __str__(self):
+        return f"Processor(name = {self.name}, transistors = {self.transistors}, year = {self.year})"
+    
 f = open("transistor-counts.csv", "r")
+processorsList = []
 lines = f.readlines()
 for line in lines:
     line = line.split(',')
     if line[0] == "#processor":
         continue
-    line[0] = str(line[0].strip())
-    line[1] = int(line[1].strip())
-    line[2] = int(line[2].strip())
-    print(line[0], line[1], line[2])
+    name, transistor, year, manufacturer, proces, area = line
+    proc = Processor(name, int(transistor), int(year))
+    processorsList.append(proc)
+    
+plt.scatter([proc.year for proc in processorsList], [proc.transistors for proc in processorsList])
+plt.yscale("log")
+plt.ylabel("Tranzistoriu skaicius")
+plt.xlabel("Isleidimo metai")
+plt.title("Muro desnis")
+plt.show()
