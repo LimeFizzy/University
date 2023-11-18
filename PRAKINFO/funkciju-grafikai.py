31a4438749df9b2f25cc6c7e7bc0f7d729a4214f
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-11-19 00:46:49 +0200

    11 pratybu prakinfo pradzia

---
+++ PRAKINFO/funkciju-grafikai.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# Pirmas etapas
+X = np.linspace(-6, 6, 500)
+Y = np.sin(pow(X,2))
+y = (-1, 1, 0.5)
+
+plt.plot(X, Y, linestyle = "dotted")
+plt.title("f(x) = sin (x^2)")
+plt.ylabel("y")
+plt.xlabel("x")
+plt.xlim(-6, 6)
+plt.yticks(np.arange(-1, 1.1, 0.5))
+plt.show()
+plt.close()
+# Pirmo etapo pabaiga
