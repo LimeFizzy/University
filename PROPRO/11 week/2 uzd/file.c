ed454b563696305c89920eeff31508752fdd787f
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-11-26 14:12:17 +0200

    11 week 2 task ready

--- PROPRO/11 week/2 uzd/file.c
+++ PROPRO/11 week/2 uzd/file.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include "file.h"
 
-void saveToFile(int array[], int capacity){
-    FILE *db = fopen("db.bin", "wb");
+int saveCount = 0;
+int loadCount = 0;
+
+void saveToFile(int array[], int capacity, char *filename){
+    FILE *db = fopen(filename, "wb");
     if(db != NULL){
         fwrite(&capacity, sizeof(int), 1, db);
         for(int i = 0; i < capacity; ++i){
             fwrite(&array[i], sizeof(int), 1, db);
         }
+        saveCount++;
     }
     fclose(db);
+    
 }
 
-void loadFromFile(int array[], int *capacity){
-    FILE* db = fopen("db.bin", "rb");
+void loadFromFile(int array[], int *capacity, char *filename){
+    FILE* db = fopen(filename, "rb");
     if(db != NULL){
         fread(capacity, sizeof(int), 1, db);
         for(int i = 0; i < *capacity; ++i){
             fread(&array[i], sizeof(int), 1, db);
         }
+        loadCount++;
     }
     fclose(db);
 }
---
+++ PROPRO/11 week/2 uzd/main.c
@@ -0,0 +1,51 @@
+#include "file.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ARR 20
+
+void fillArray(int array[], int capacity){
+    srand(time(NULL));
+    for(int i = 0; i < capacity; ++i){
+        array[i] = 1 + rand()% 100;
+    }
+}
+
+void printArray(int array[], int capacity){
+    for(int i = 0; i < capacity; ++i){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+
+int main(){
+    int *m1 = NULL, *m2 = NULL, *m3 = NULL;
+    int cap;
+    m1 = malloc(MAX_ARR * sizeof(int));
+    m2 = malloc(MAX_ARR * sizeof(int));
+    m3 = malloc(MAX_ARR * sizeof(int));
+    
+    fillArray(m1, MAX_ARR);
+    fillArray(m2, MAX_ARR);
+    fillArray(m3, MAX_ARR);
+
+    saveToFile(m1, MAX_ARR, "db1.bin");
+    saveToFile(m3, MAX_ARR, "db1.bin");
+    
+    loadFromFile(m1, &cap, "db1.bin");
+    
+    saveToFile(m2, MAX_ARR, "db2.bin");
+    loadFromFile(m3, &cap, "db2.bin");
+    loadFromFile(m2, &cap, "db1.bin");
+    
+    if(saveCount == 3 && loadCount == 3){
+        printf("all good\n");
+    }
+    else{
+        printf("something's wrong\n");
+    }
+    return 0;
+}
