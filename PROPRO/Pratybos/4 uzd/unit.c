ebc2382471fadb9ed32427bfd898ee9fbf2e4bf9
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2023-12-03 13:22:35 +0200

    4 pratybu uzduotis paruosta

--- PROPRO/Pratybos/4 uzd/unit.c
+++ PROPRO/Pratybos/4 uzd/unit.c
@@ -2,67 +2,43 @@
 #include <assert.h>
 #include "func.c"
 
-void test_PrintList() {
-    // Test case 1: Add your test here
-    // Example:
-    // Node *head = ...; // Create or initialize your linked list
-    // printf("Expected output: ...\n");
-    // printf("Actual output: ");
-    // PrintList(head);
-    // assert(0); // Replace 0 with your condition based on the test case
-}
-
 void test_FindMaxValue() {
-    // Test case 1: Add your test here
-    // Example:
-    // Node *head = ...; // Create or initialize your linked list
-    // int expected_max = ...; // Define the expected maximum value
-    // int actual_max = FindMaxValue(head);
-    // printf("Expected max: %d\n", expected_max);
-    // printf("Actual max: %d\n", actual_max);
-    // assert(expected_max == actual_max);
+    Node *start = NULL, *end = NULL;
+    ReadList(&start, &end);
+    assert(FindMaxValue(start) == 456);
 }
 
 void test_DeleteMaxValue() {
-    // Test case 1: Add your test here
-    // Example:
-    // Node *head = ...; // Create or initialize your linked list
-    // int value_to_delete = ...; // Define the value to delete
-    // DeleteMaxValue(&head, value_to_delete);
-    // printf("Expected linked list after deletion: ...\n");
-    // printf("Actual linked list after deletion: ");
-    // PrintList(head);
-    // assert(0); // Replace 0 with your condition based on the test case
+    Node *start = NULL, *end = NULL;
+    ReadList(&start, &end);
+    DeleteMaxValue(&start, 456);
+    printf("Tiketinas sarasas po didziausio elemento naikinimo: 45 90 12 12 1 56 32 99 123 78 56 1 2 9.\n");
+    printf("Duomenys kurius gavome: ");
+    PrintList(start);
+    printf("\n");
 }
 
 void test_ReadList() {
-    // Test case 1: Add your test here
-    // Example:
-    // Node *start, *end;
-    // ReadList(&start, &end);
-    // printf("Expected linked list from file: ...\n");
-    // printf("Actual linked list from file: ");
-    // PrintList(start);
-    // assert(0); // Replace 0 with your condition based on the test case
+    Node *start = NULL, *end = NULL;
+    ReadList(&start, &end);
+    printf("Tiketini duomenys is failo: 45 90 12 456 12 1 56 32 99 123 78 56 1 2 9.\n");
+    printf("Duomenys kurius gavome: ");
+    PrintList(start);
+    printf("\n");
 }
 
 void test_Validation() {
-    // Test case 1: Add your test here
-    // Example:
-    // char input = ...; // Define the input character
-    // int result = Validation(input);
-    // printf("Expected result: ...\n");
-    // printf("Actual result: %d\n", result);
-    // assert(0); // Replace 0 with your condition based on the test case
+    assert(Validation('y') == 1);
+    assert(Validation('n') == -1);
+    assert(Validation(' ') == 0);
 }
 
 int main() {
-    test_PrintList();
     test_FindMaxValue();
     test_DeleteMaxValue();
     test_ReadList();
     test_Validation();
 
-    printf("All tests passed!\n");
+    printf("Visi testai sekmingi.\n");
     return 0;
 }
